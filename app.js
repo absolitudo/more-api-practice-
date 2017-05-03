ebbc762567a752fc6791cd473b229bbf73877edf
absolitudo <absolitudo@gmail.com>
2017-05-03 17:52:37 +0200

    backend

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require("express"),
+    app = express();
+app.use(express.static("./static"));
+app.get("/api/whoami", function(req, res) {
+    res.send({
+        "ipaddress": req.headers.host,
+        "language": req.headers["accept-language"].slice(0, 5),
+        "software": req.headers["user-agent"]
+    });
+});
+app.listen(process.env.PORT || 8080);
+console.log("server up");
